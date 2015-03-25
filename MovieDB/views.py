991870c0e9fac6aac62d9b5892cf88088d494270
Parikshit Juluri <pari685@gmail.com>
2015-03-24 22:48:11 -0500

    Movie already exists

--- MovieDB/views.py
+++ MovieDB/views.py
@@ -18,15 +18,6 @@ def index(request):
     return render_to_response('index.html')
 
 
-#class IndexTemplate(TemplateView):
-#    template_name = "index_class.html"
-#    def get_context_data(self, **kwargs):
-#        context = super(IndexTemplate, self).get_context_data(**kwargs)
-#        context['name'] = 'Parikshit'
-#        return context
-
-
-
 def results(request, movie_id):
     data = Movie.objects.get(id=movie_id)
     title = data.title
@@ -37,8 +28,13 @@ def add_movie_form(request):
     if request.POST:
         form = MovieForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('.')
+            # Check if the movie already exists in the database
+            check_db = Movie.objects.filter(title=request.POST['title'])
+            if len(check_db) > 0:
+                return render(request, 'movie_exists.html', {'movie_title': request.POST['title']})
+            else:
+                form.save()
+                return HttpResponseRedirect('.')
     else:
         form = MovieForm()
     return render(request, 'add_movie_form.html',
--- movies/urls.py
+++ movies/urls.py
@@ -7,8 +7,6 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
                        url(r'^$', 'MovieDB.views.index'),
                        url(r'^(?P<movie_id>\d+)/results/$', 'MovieDB.views.results'),
-                       #url(r'^moviedb/results/$', 'MovieDB.views.results'),
                        url(r'^addmovieform/$', 'MovieDB.views.add_movie_form'),
-                       #url(r'^addmovieform/addmovie/$', 'MovieDB.views.add_movie'),
                        url(r'^admin/', include(admin.site.urls)),
                        )
