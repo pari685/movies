c01f1ee822c93234d189b6b6a091de544514fd67
Parikshit Juluri <pari685@gmail.com>
2015-03-29 19:08:09 -0500

    Added the GNU License

--- MovieDB/views.py
+++ MovieDB/views.py
@@ -1,4 +1,23 @@
-""" The views for the SImple MOvie Database  """
+#!/usr/bin/env python
+#
+# A Simple Movie Database
+# Copyright (C) 2015, Parikshit Juluri
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+""" The views for the Simple MOvie Database  """
 from django.shortcuts import render_to_response, render
 from forms import MovieForm
 from MovieDB.models import Movie
